157595561f625f06f63ff8a5757fad60398a7a5d
Marcos Pascoski <marcospascoski@gmail.com>
2018-11-16 15:02:57 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/marcospascoski/cursomc/resources/CategoriaResourse.java
+++ src/main/java/com/marcospascoski/cursomc/resources/CategoriaResourse.java
@@ -22,7 +22,7 @@ public class CategoriaResourse {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
@@ -42,4 +42,9 @@ public class CategoriaResourse {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/marcospascoski/cursomc/services/CategoriaService.java
+++ src/main/java/com/marcospascoski/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.marcospascoski.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.marcospascoski.cursomc.domain.Categoria;
 import com.marcospascoski.cursomc.repositories.CategoriaRepository;
+import com.marcospascoski.cursomc.services.exceptions.DataIntegrityException;
 import com.marcospascoski.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
