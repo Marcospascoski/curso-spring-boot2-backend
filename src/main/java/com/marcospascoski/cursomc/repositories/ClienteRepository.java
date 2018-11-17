5721f9d432294e447d90f8a6d00cf2c6970e5b81
Marcos Pascoski <marcospascoski@gmail.com>
2018-11-17 12:38:59 -0300

    Validação customizada: email não repetido na inserção de cliente

--- src/main/java/com/marcospascoski/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/marcospascoski/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.marcospascoski.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.marcospascoski.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
+	
 }
