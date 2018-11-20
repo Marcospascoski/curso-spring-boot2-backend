acb478b9c844a3798fc90ddf42d0e0aa7200550b
Marcos Pascoski <marcospascoski@gmail.com>
2018-11-20 15:57:42 -0300

    Esqueci a senha

--- src/main/java/com/marcospascoski/cursomc/services/EmailService.java
+++ src/main/java/com/marcospascoski/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.marcospascoski.cursomc.domain.Cliente;
 import com.marcospascoski.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -16,4 +17,6 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
