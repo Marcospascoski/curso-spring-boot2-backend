cee1a408428559e532601cdf5bb6c37ccdbb40e6
Marcos Pascoski <marcospascoski@gmail.com>
2018-11-21 19:06:36 -0300

    Salvando URL da imagem em Cliente

--- src/main/java/com/marcospascoski/cursomc/domain/Cliente.java
+++ src/main/java/com/marcospascoski/cursomc/domain/Cliente.java
@@ -56,6 +56,8 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
+	private String imageUrl;
+	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -175,4 +177,14 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+	
 }
